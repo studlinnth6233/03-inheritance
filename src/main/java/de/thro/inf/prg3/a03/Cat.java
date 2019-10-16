edd49ebb0a8c612dd70a5ffd04f648d7935c29f5
Linner-Thomas <linner.thomas@gmx.net>
2019-10-16 09:43:53 +0200

    Imported Project, Reformatting

--- src/main/java/de/thro/inf/prg3/a03/Cat.java
+++ src/main/java/de/thro/inf/prg3/a03/Cat.java
@@ -5,112 +5,136 @@ import org.apache.logging.log4j.Logger;
 
 import static de.thro.inf.prg3.a03.Cat.State.*;
 
-public class Cat {
-	private static final Logger logger = LogManager.getLogger();
+public class Cat
+{
+    private static final Logger logger = LogManager.getLogger();
 
-	// valid states
-	public enum State {SLEEPING, HUNGRY, DIGESTING, PLAYFUL, DEAD}
+    // valid states
+    public enum State
+    {
+        SLEEPING, HUNGRY, DIGESTING, PLAYFUL, DEAD
+    }
 
-	// initially, animals are sleeping
-	private State state = State.SLEEPING;
+    // initially, animals are sleeping
+    private State state = State.SLEEPING;
 
-	// state durations (set via constructor), ie. the number of ticks in each state
-	private final int sleep;
-	private final int awake;
-	private final int digest;
+    // state durations (set via constructor), ie. the number of ticks in each state
+    private final int sleep;
+    private final int awake;
+    private final int digest;
 
-	private final String name;
+    private final String name;
 
-	private int time = 0;
-	private int timeDigesting = 0;
+    private int time = 0;
+    private int timeDigesting = 0;
 
-	public Cat(String name, int sleep, int awake, int digest) {
-		this.name = name;
-		this.sleep = sleep;
-		this.awake = awake;
-		this.digest = digest;
-	}
+    public Cat(String name, int sleep, int awake, int digest)
+    {
+        this.name = name;
+        this.sleep = sleep;
+        this.awake = awake;
+        this.digest = digest;
+    }
 
-	public void tick(){
-		logger.info("tick()");
-		time = time + 1;
+    public void tick()
+    {
+        logger.info("tick()");
+        time = time + 1;
 
-		switch (state) {
-			case SLEEPING:
-				if (time == sleep) {
-					logger.info("Yoan... getting hungry!");
-					state = HUNGRY;
-					time = 0;
-				}
-				break;
-			case HUNGRY:
-				if(time == awake){
-					logger.info("I've starved for a too long time...good bye...");
-					state = DEAD;
-				}
-				break;
-			case DIGESTING:
-				timeDigesting = timeDigesting + 1;
-				if (timeDigesting == digest) {
-					logger.info("Getting in a playful mood!");
-					state = PLAYFUL;
-				}
-				break;
-			case PLAYFUL:
-				if (time >= awake) {
-					logger.info("Yoan... getting tired!");
-					state = SLEEPING;
-					time = 0;
-				}
-				break;
+        switch (state)
+        {
+            case SLEEPING:
+                if (time == sleep)
+                {
+                    logger.info("Yoan... getting hungry!");
+                    state = HUNGRY;
+                    time = 0;
+                }
+                
+                break;
 
-			case DEAD:
-				break;
-			default:
-				throw new IllegalStateException("Unknown cat state " + state.name());
-		}
+            case HUNGRY:
+                if (time == awake)
+                {
+                    logger.info("I've starved for a too long time...good bye...");
+                    state = DEAD;
+                }
 
-		logger.info(state.name());
+                break;
 
-	}
+            case DIGESTING:
+                timeDigesting = timeDigesting + 1;
 
-	/**
-	 * This would be a user interaction: feed the cat to change its state!
-	 */
-	public void feed(){
-		if (!isHungry())
-			throw new IllegalStateException("Can't stuff a cat...");
+                if (timeDigesting == digest)
+                {
+                    logger.info("Getting in a playful mood!");
+                    state = PLAYFUL;
+                }
 
-		logger.info("You feed the cat...");
+                break;
 
-		// change state and reset the timer
-		state = State.DIGESTING;
-		timeDigesting = 0;
-	}
+            case PLAYFUL:
+                if (time >= awake)
+                {
+                    logger.info("Yoan... getting tired!");
+                    state = SLEEPING;
+                    time = 0;
+                }
+                break;
 
-	public boolean isAsleep() {
-		return state.equals(State.SLEEPING);
-	}
+            case DEAD:
+                break;
 
-	public boolean isPlayful() {
-		return state.equals(State.PLAYFUL);
-	}
+            default:
+                throw new IllegalStateException("Unknown cat state " + state.name());
+        }
 
-	public boolean isHungry() {
-		return state.equals(State.HUNGRY);
-	}
+        logger.info(state.name());
+    }
 
-	public boolean isDigesting() {
-		return state.equals(State.DIGESTING);
-	}
+    /**
+     * This would be a user interaction: feed the cat to change its state!
+     */
+    public void feed()
+    {
+        if (!isHungry())
+            throw new IllegalStateException("Can't stuff a cat...");
 
-	public boolean isDead() {
-		return state == State.DEAD;
-	}
+        logger.info("You feed the cat...");
 
-	@Override
-	public String toString() {
-		return name;
-	}
+        // change state and reset the timer
+        state = State.DIGESTING;
+        timeDigesting = 0;
+    }
 
+    public boolean isAsleep()
+    {
+        return state.equals(State.SLEEPING);
+    }
+
+    public boolean isPlayful()
+    {
+        return state.equals(State.PLAYFUL);
+    }
+
+    public boolean isHungry()
+    {
+        return state.equals(State.HUNGRY);
+    }
+
+    public boolean isDigesting()
+    {
+        return state.equals(State.DIGESTING);
+    }
+
+    public boolean isDead()
+    {
+        return state == State.DEAD;
+    }
+
+    @Override
+    public String toString()
+    {
+        return name;
+    }
 }
--- src/test/java/de/thro/inf/prg3/a03/tests/CatTest.java
+++ src/test/java/de/thro/inf/prg3/a03/tests/CatTest.java
@@ -6,65 +6,70 @@ import org.junit.jupiter.api.Test;
 
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Peter Kurfer
  * Created on 10/7/17.
  */
-public class CatTest {
+public class CatTest
+{
 
-	private static final int Sleep = 10;
-	private static final int Awake = 15;
-	private static final int Digest = 10;
+    private static final int Sleep = 10;
+    private static final int Awake = 15;
+    private static final int Digest = 10;
 
-	private Cat animal;
+    private Cat animal;
 
-	@BeforeEach
-	void setUp() {
-		animal = new Cat("Horst", Sleep, Awake, Digest);
-	}
+    @BeforeEach
+    void setUp()
+    {
+        animal = new Cat("Horst", Sleep, Awake, Digest);
+    }
 
-	@Test
-	void testFeedHungryAnimal() {
-		IntStream.range(0, Sleep).forEach(i -> animal.tick());
-		assertTrue(animal.isHungry());
-		animal.feed();
-		assertTrue(animal.isDigesting());
-	}
+    @Test
+    void testFeedHungryAnimal()
+    {
+        IntStream.range(0, Sleep).forEach(i -> animal.tick());
+        assertTrue(animal.isHungry());
+        animal.feed();
+        assertTrue(animal.isDigesting());
+    }
 
-	@Test
-	void testFeedNotHungryAnimal(){
-		assertFalse(animal.isHungry());
-		assertThrows(IllegalStateException.class, () -> animal.feed());
-	}
+    @Test
+    void testFeedNotHungryAnimal()
+    {
+        assertFalse(animal.isHungry());
+        assertThrows(IllegalStateException.class, () -> animal.feed());
+    }
 
-	@Test
-	void testAnimalGettingPlayful() {
-		//Waiting while animal is sleeping
-		IntStream.range(0, Sleep).forEach(i -> animal.tick());
-		animal.feed();
-		//Waiting while animal is digesting
-		IntStream.range(0, Digest).forEach(i -> animal.tick());
-		assertTrue(animal.isPlayful());
-	}
+    @Test
+    void testAnimalGettingPlayful()
+    {
+        //Waiting while animal is sleeping
+        IntStream.range(0, Sleep).forEach(i -> animal.tick());
+        animal.feed();
+        //Waiting while animal is digesting
+        IntStream.range(0, Digest).forEach(i -> animal.tick());
+        assertTrue(animal.isPlayful());
+    }
 
-	@Test
-	void testAnimalDying(){
-		//Waiting while animal is sleeping and awake before it's going to die
-		IntStream.range(0, Sleep + Awake).forEach(i -> animal.tick());
-		assertTrue(animal.isDead());
-	}
+    @Test
+    void testAnimalDying()
+    {
+        //Waiting while animal is sleeping and awake before it's going to die
+        IntStream.range(0, Sleep + Awake).forEach(i -> animal.tick());
+        assertTrue(animal.isDead());
+    }
 
-	@Test
-	void testAnimalIsGoingToSleepAgain(){
-		//Waiting while animal is sleeping
-		IntStream.range(0, Sleep).forEach(i -> animal.tick());
-		animal.feed();
-		//Waiting while animal is digesting and in playful mood
-		IntStream.range(0, Awake).forEach(i -> animal.tick());
-		assertTrue(animal.isAsleep());
-	}
+    @Test
+    void testAnimalIsGoingToSleepAgain()
+    {
+        //Waiting while animal is sleeping
+        IntStream.range(0, Sleep).forEach(i -> animal.tick());
+        animal.feed();
+        //Waiting while animal is digesting and in playful mood
+        IntStream.range(0, Awake).forEach(i -> animal.tick());
+        assertTrue(animal.isAsleep());
+    }
 }
